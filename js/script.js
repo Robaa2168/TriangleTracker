37e3e34354ee95419135cca2d80ddf9a85d48a37
rockstarchiku <jngamau93@gmail.com>
2017-04-30 20:03:15 +0300

    Added condition to output required results

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+$(document).ready(function() {
+      $("form").submit(function(event) {
+        var a = parseInt($("input#lengtha").val());
+        var b = parseInt($("input#lengthb").val());
+        var c = parseInt($("input#lengthc").val());
+        var results="";
+
+
+
+      if(c> a+b || b>a+c || a>b+c){
+        results = "Not a Triangle";
+      }else {
+        if(a===b && a===c){
+          results = "Equilateral";
+        }else {
+          if(a===b || a===c || b===c ){
+          results = "Isosceles";
+        }
+        else {
+          if(a!=b && a!=c){
+             results = "Scalene";
+          }
+        }
+      }
+      }
+        $(".output").text(results);
+        event.preventDefault();
+      });
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
@@ -6,3 +36,4 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+});
